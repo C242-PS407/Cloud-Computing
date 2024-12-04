a8ecbf94ae84c84f1604f1b27e834709ee1cc993
bukan-tapio <pio.tapio90@gmail.com>
2024-12-04 11:41:16 +0700

    update : file uploadFiles.js and index.js

--- api-authentication/src/index.js
+++ api-authentication/src/index.js
@@ -115,7 +115,7 @@ app.get("/user", authenticateToken, (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () => {
   console.log("server is running on port: " + PORT);
 });
